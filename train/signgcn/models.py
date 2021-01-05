1db5d1e34f127b801abdd03df53c241d924ec0eb
matbun <matteo.bunino@gmail.com>
2021-01-05 19:09:48 +0000

    colab_commit

--- train/signgcn/models.py
+++ train/signgcn/models.py
@@ -3,6 +3,9 @@ import torch
 import torch.nn.functional as F
 from signgcn.layers import SIGNGraphConvolution
 
+def init_linear(m):
+  m.weight.data.normal_(0, 0.01)
+  m.bias.data.normal_(0, 0.01)
 
 class SIGN(nn.Module):
     def __init__(self, nfeat, nhid, nclass, nlayers, dropout, init="kipf"):
@@ -21,6 +24,7 @@ class SIGN(nn.Module):
           )
         # Final linear layer
         self.omega = nn.Linear(nlayers*nhid, nclass)
+        self.omega.apply(init_linear)
         self.dropout = dropout
 
     def forward(self, precomp_ax_list):
@@ -31,12 +35,13 @@ class SIGN(nn.Module):
         prev_x = None
         for i, ax in enumerate(precomp_ax_list):
           x = self.gcs[i](ax)
+          #x = F.dropout(x, self.dropout, training=self.training)
           # concat
           if prev_x is not None:
             x = torch.cat([prev_x, x], dim=1)
           prev_x = x
 
         x = F.relu(x)
-        #x = F.dropout(x, self.dropout, training=self.training)
+        x = F.dropout(x, self.dropout, training=self.training)
         x = self.omega(x)
         return F.log_softmax(x, dim=1)
